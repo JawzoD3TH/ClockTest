1a552c4153069862fa2c13bc2c90402fd6d7b7bc
Jawid Hassim <hassimjawid@gmail.com>
2018-07-14 12:47:32 +0200

    Add project files.

---
+++ ClockTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ClockTest
+{
+    class Program
+    {
+        const char Period = '.';
+        const int Time = 8500;
+        const int Timezone = 2;
+        static Timer timer = new Timer(Time);
+        static ConcurrentBag<DateTime> DateTimes = new ConcurrentBag<DateTime>();
+        
+        static void Main(string[] args)
+        {
+            timer.Elapsed += new ElapsedEventHandler(Timer_Elapsed);
+            timer.Enabled = true;
+            while (timer.Enabled)
+                Task.Run(() => DateTimes.Add(DateTime.UtcNow.AddHours(Timezone)));
+
+            int Count = DateTimes.Count;
+            Console.WriteLine($"Asyncronous: {DateTimes.Count}");
+
+            timer.Close();
+            timer.Dispose();
+            timer = new Timer(Time);
+            timer.Elapsed += new ElapsedEventHandler(Timer_Elapsed);
+            DateTimes.Clear();
+            timer.Enabled = true;
+            while (timer.Enabled)
+                DateTimes.Add(DateTime.UtcNow.AddHours(Timezone));
+
+            Console.WriteLine($"Syncronous:  {DateTimes.Count}\r\n{DateTimes.Count / Count}x Faster");
+            DateTimes.Clear();
+            timer.Close();
+            timer.Dispose();
+            Console.ReadKey();
+        }
+
+        static void Timer_Elapsed(object sender, ElapsedEventArgs e) { timer.Enabled = false; }
+    }
+}
